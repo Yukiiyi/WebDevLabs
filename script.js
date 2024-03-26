9b7795de6aa21c6f511133b8c93a14b545d3c64d
Yukiiyi <138192206+Yukiiyi@users.noreply.github.com>
2024-03-26 16:22:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+function greetingFunc() {
+    var d = new Date();
+    var h = d.getHours();
+    var E = document.getElementById("greeting");
+    if (h>=5 && h<12) {
+        E.innerHTML=" Good morning, I am Yuqi";
+    } else if (h>=12 && h<18) {
+        E.innerHTML+=" Good afternoon, I am Yuqi";
+    } else if (h>=18 && h<20) {
+        E.innerHTML=" Good evening, I am Yuqi";
+    } else  {
+        E.innerHTML="Good night, I am Yuqi";
+    } }
+ //greetingFunc();
+
+var L = window.location.href;
+console.log(L);
+if (L.includes("index.html")) {
+   greetingFunc();
+}
+
+function addYear() {
+    var d= new Date();
+    var y = d.getFullYear();
+    var E = document.getElementById("copyYear");
+    E.innerHTML+=y;
+ } 
+
+ function showList() {
+    document.getElementById("FavList").style.display = "block";
+    document.getElementById("SeeMoreBTN").style.display = "none";
+ } 
+
+ $("#readLess").click(function(){
+    $("#longIntro").hide();
+    $("#readLess").hide();   
+    $("#readMore").show();      
+
+  });
+  
+  $("#readMore").click(function(){
+    $("#longIntro").show();
+    $("#readLess").show();
+    $("#readMore").hide();      
+  });
+ 
+  function validate() {
+    var userName = document.getElementById("UserName");
+    var userEmail = document.getElementById("UserEmail");
+    var userText = document.getElementById("Usertext");
+    var msg = document.getElementById("ValidateMsg");
+    if (!userName.checkValidity() || !userEmail.checkValidity() || !userText.checkValidity()) {
+        msg.innerHTML = "Please fill out the form correctly so I can get back to you :)";
+    }
+ } 
+
+// Initialize and add the map
+
+let map;
+async function initMap() {
+ // The location of CMU
+ const position = { lat: 42.81336547146971, lng:140.68752278935824 };
+ const { Map } = await google.maps.importLibrary("maps");
+ const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+ map = new Map(document.getElementById("map"), {
+   zoom: 12,
+   center: position,
+   mapId: "DEMO_MAP_ID",
+ });
+
+ const marker = new AdvancedMarkerElement({
+   map: map,
+   position: position,
+   title: "Hokkaido",
+ });
+
+}
+
+
+initMap();
+
+function activeNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
